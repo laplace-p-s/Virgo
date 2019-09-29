dae1aa08e931d49bbd6927f55da64b4c39a2c101
laplace_p_s <laplace2012sao@gmail.com>
2019-09-29 12:19:32 +0900

    現在時刻を表現する処理を実装

--- Virgo/Virgo/MainForm.Designer.cs
+++ Virgo/Virgo/MainForm.Designer.cs
@@ -28,23 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.TimerDayLabel = new System.Windows.Forms.Label();
+            this.TimerTimeLabel = new System.Windows.Forms.Label();
+            this.CountTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // TimerDayLabel
+            // 
+            this.TimerDayLabel.AutoSize = true;
+            this.TimerDayLabel.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TimerDayLabel.Location = new System.Drawing.Point(85, 24);
+            this.TimerDayLabel.Name = "TimerDayLabel";
+            this.TimerDayLabel.Size = new System.Drawing.Size(197, 19);
+            this.TimerDayLabel.TabIndex = 0;
+            this.TimerDayLabel.Text = "yyyy年 MM月 dd日 (月)";
+            // 
+            // TimerTimeLabel
+            // 
+            this.TimerTimeLabel.AutoSize = true;
+            this.TimerTimeLabel.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TimerTimeLabel.Location = new System.Drawing.Point(133, 57);
+            this.TimerTimeLabel.Name = "TimerTimeLabel";
+            this.TimerTimeLabel.Size = new System.Drawing.Size(90, 21);
+            this.TimerTimeLabel.TabIndex = 1;
+            this.TimerTimeLabel.Text = "hh:mm:ss";
+            // 
+            // CountTimer
+            // 
+            this.CountTimer.Tick += new System.EventHandler(this.CountTimer_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(369, 225);
+            this.Controls.Add(this.TimerTimeLabel);
+            this.Controls.Add(this.TimerDayLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
             this.Name = "MainForm";
             this.Text = "Virgo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label TimerDayLabel;
+        private System.Windows.Forms.Label TimerTimeLabel;
+        private System.Windows.Forms.Timer CountTimer;
     }
 }
 
--- Virgo/Virgo/MainForm.cs
+++ Virgo/Virgo/MainForm.cs
@@ -15,6 +15,19 @@ namespace Virgo
         public MainForm()
         {
             InitializeComponent();
+            CountTimer.Start();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //読み込み時のフォームセットアップ
+        }
+
+        private void CountTimer_Tick(object sender, EventArgs e)
+        {
+            DateTime nowTime = DateTime.Now;
+            TimerDayLabel.Text  = nowTime.ToString("yyyy年 MM月 dd日 (ddd)");
+            TimerTimeLabel.Text = nowTime.ToLongTimeString();
         }
     }
 }
