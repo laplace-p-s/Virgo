1fdbc1ae157117d4665fad9f064bd7af0815ff06
laplace_p_s <laplace2012sao@gmail.com>
2020-04-04 17:04:18 +0900

    起動時に設定保持用のDBファイルを生成する処理を追加

---
+++ Virgo/Virgo/Dao/DaoSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Virgo
+{
+    class DaoSettings
+    {
+        private const string DB_FILE_NAME = "virgo_settings.db";
+        private static string TABLE_NAME = "Settings";
+
+        public DaoSettings()
+        {
+            //コンストラクタ
+        }
+
+        public static string Setup()
+        {
+            string errMes = "";
+            try
+            {
+                using (SQLiteConnection con = new SQLiteConnection("Data Source=" + DB_FILE_NAME))
+                {
+                    con.Open();
+                    using (SQLiteCommand cmd = con.CreateCommand())
+                    {
+                        StringBuilder sql = new StringBuilder();
+                        sql.AppendLine("CREATE TABLE IF NOT EXISTS " + TABLE_NAME);
+                        sql.AppendLine("(");
+                        sql.AppendLine(" name TEXT,");
+                        sql.AppendLine(" param INTEGER");
+                        sql.AppendLine(")");
+
+                        cmd.CommandText = sql.ToString();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                errMes = e.ToString();
+            }
+
+            return errMes;
+        }
+    }
+}
