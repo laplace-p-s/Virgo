267ec67489c86336c46821cb7c5786501c681a69
laplace_p_s <laplace2012sao@gmail.com>
2022-11-18 12:51:12 +0900

    コピーライト表記を開始年のみに変更

--- Virgo/Virgo/Properties/AssemblyInfo.cs
+++ Virgo/Virgo/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Virgo")]
-[assembly: AssemblyCopyright("Copyright ©  2019-2022 laplace_p_s LaplaceLab.")]
+[assembly: AssemblyCopyright("Copyright ©  2019 laplace_p_s LaplaceLab.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
